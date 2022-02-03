4cd0fe4a754c73a97ff4356306fa9265d3a7d7fe
rumblerick <rick@scrumble.nl>
2022-02-03 15:43:10 +0100

    Revert "Episode 3 - Making use of state"
    This reverts commit 6280eea0117dbad64d1fbfbf9b2fca8908330c79.

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-import AppClass from './components/AppClass';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <AppClass />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
