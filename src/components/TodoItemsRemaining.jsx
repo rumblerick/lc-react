ef24ca46cbed8b630e0d2f0d166d6b336eb04fb3
rumblerick <rick@scrumble.nl>
2022-02-03 15:42:28 +0100

    Revert "Episode 8 - Other Built-in React Hooks"
    This reverts commit 3b23f4198d15450899759f339223cf08c92534b3.

--- src/components/TodoItemsRemaining.jsx
+++ src/components/TodoItemsRemaining.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 TodoItemsRemaining.propTypes = {
-  remaining: PropTypes.number.isRequired,
+  remaining: PropTypes.func.isRequired,
 };
 
 function TodoItemsRemaining(props) {
-  return <span>{props.remaining} items remaining</span>;
+  return <span>{props.remaining()} items remaining</span>;
 }
 
 export default TodoItemsRemaining;
